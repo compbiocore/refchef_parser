b33514db77ec65034b0333ca488d15b4e85e97a8
Joselynn Wallace <joselynnw@gmail.com>
2022-05-13 15:15:11 -0400

    updating parser after github actions

--- parser/parser.py
+++ parser/parser.py
@@ -1,61 +1,69 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import oyaml as yaml
 import csv
-import pandas as pd
+# import pandas as pd
+import json
+import ast
 
 # make an empty dictionary:
 
 
-file = csv.reader('/Users/jwalla12/Repositories/refchef_parser/tests/data/new_references.csv')
+#file = csv.reader('/Users/jwalla12/Repositories/cbc-references-refchef/new_ref.yaml')
 
-new_keys = ['timestamp', 'command', 'name', 'common', 'taxon_id', 'organism', 'organization', 'custom', 'description',
-            'ensembl_rel', 'genbank', 'refseq', 'component']
-
-with open('/Users/jwalla12/Repositories/refchef_parser/tests/data/new_references.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    ref_list = []
-    for row in reader:
-        ref_list.append(row)
-    new_list = []
-    for d in ref_list:
-        zipped = zip(new_keys, d.values())
-        new_dict = {}
-        for k, v in zipped:
-            new_dict[k] = v
-        new_list.append(new_dict)  # just incase we have more than 1 entry at a time....
-    print(new_list)
-
-# new_dict will work for now bc there's only 1 dict but would otherwise need to do a for loop on the new list
-new_dict = new_list[0]
-yaml_name = new_dict.get('name')
-yaml_dict = {
-    new_dict.get('name'):
-        {'metadata':
-             {'name': new_dict.get('name'),
-              'common_name': new_dict.get('common'),
-              'custom': new_dict.get('custom'),
-              'description': new_dict.get('description'),
-              'downloader': 'joselynn wallace',
-              'ncbi_taxon_id': new_dict.get('taxon_id'),
-              'ensembl_release_number': new_dict.get('ensembl_rel'),
-              'accession':
-                  {'genbank': new_dict.get('genbank'),
-                   'refseq': new_dict.get('refseq')},
-              'organism': new_dict.get('organism'),
-              'organization': new_dict.get('organization')
-              },
-         'levels':
-             {'references':
-                 [
-                     {
-                         'component': new_dict.get('component'),
-                         'commands': [new_dict.get('command')]
-                     }
-                 ]
+#new_keys = ['timestamp', 'command', 'name', 'common', 'taxon_lsid', 'organism', 'organization', 'custom', 'description',
+            # 'ensembl_rel', 'genbank', 'refseq', 'component']
+with open('/Users/jwalla12/Repositories/cbc-references-refchef/new_ref.yaml') as f:
+    data = f.read()
+    datajson = json.loads(data)
+    yaml_level = datajson.get('level')
+    yaml_commands = datajson.get('Commands')
+    print(yaml_commands)
+    print(datajson)
+    yaml_dict = {
+        datajson.get('name'):
+            {'metadata':
+                 {'name': datajson.get('name'),
+                  'common_name': datajson.get('common'),
+                  'custom': datajson.get('custom'),
+                  'description': datajson.get('description'),
+                  'downloader': 'joselynn wallace',
+                  'ncbi_taxon_id': datajson.get('taxon_id'),
+                  'ensembl_release_number': datajson.get('ensembl_rel'),
+                  'accession':
+                      {'genbank': datajson.get('genbank'),
+                       'refseq': datajson.get('refseq')},
+                  'organism': datajson.get('organism'),
+                  'organization': datajson.get('organization'),
+                  'category': datajson.get('category')
+                  },
+                'levels':
+                    {yaml_level:
+                     [
+                         {
+                             'component': datajson.get('component'),
+                             'commands': [datajson.get('Commands')]
+                         }
+                     ]
+                 }
              }
-         }
-}
-f = open('/Users/jwalla12/Repositories/refchef_parser/tests/data/test.yaml', 'w+')
-yaml.dump(yaml_dict, f, allow_unicode=True)
+    }
+    out = open('/Users/jwalla12/Repositories/cbc-references-refchef/new_parsed.yaml', 'w+')
+    yaml.dump(yaml_dict, out, allow_unicode=True)
+
+
+    #          'levels':
+    #              {'references':
+    #                  [
+    #                      {
+    #                          'component': datajson.get('component'),
+    #                          'commands': [datajson.get('command')]
+    #                      }
+    #                  ]
+    #              }
+    #          }
+    # }
+    #f = open('/Users/jwalla12/Repositories/refchef_parser/tests/data/test.yaml', 'w+')
+    #yaml.dump(yaml_dict, f, allow_unicode=True)
